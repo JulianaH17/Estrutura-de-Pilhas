0629568b53c1a2391b0f2d6e1e4eaee174009a1b
Isabela <juliana.bando@edu.unifil.br>
2026-03-12 15:30:49 -0300

    Implementação dos métodos da PilhaSimples (precisam de correção) e testes na Main

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
+        PilhaSimples pilhaSimples = new PilhaSimples(5);
 
+        pilhaSimples.empilhar("Lucas");
+        pilhaSimples.empilhar("Vitor");
+        pilhaSimples.empilhar("Leticia");
+        pilhaSimples.empilhar("Caio");
+        pilhaSimples.empilhar("Nicolas");
+        pilhaSimples.desempilhar(); //verificar pq o primeiro elemento da lista tá duplicado quando retira o último
+
+        pilhaSimples.exibirElementos();
     }
 }
--- src/PilhaSimples.java
+++ src/PilhaSimples.java
@@ -1,3 +1,59 @@
 public class PilhaSimples {
+    String [] pilha;
+
+    //Construtor para inicializar a pilha dependendo do tamanho informado pelo usuário
+    public PilhaSimples(int tamanho){
+        this.pilha = new String[tamanho];
+        System.out.println("A pilha foi criada com sucesso! Existem " + tamanho + " espaços disponíveis.");
+    }
+
+    //Função que insere elementos na pilha
+    public void empilhar(String elemento){
+        if (!this.estaCheia()){
+            this.pilha[encontrarPosicao()] = elemento;
+            System.out.println("Elemento inserido com sucesso: " + elemento);
+        }
+        else{
+            System.out.println("Não foi possivel inserir o elemento " + elemento);
+        }
+    }
+
+    //Função que remove elementos da pilha (último --> primeiro) tem q arrumar esse!
+    public void desempilhar(){
+        if (this.estaCheia()){  //se ela estiver cheia
+            for (int i = this.pilha.length-1; i > 0; i--){
+                this.pilha[i] = this.pilha[i-1];
+            }
+        }
+    }
+
+    //Função para verificar se a pilha tá cheia ou não
+    public boolean estaCheia() {
+        for (int i = 0; i < this.pilha.length; i++) {
+            if(this.pilha[i] == null) {
+                return false;
+            }
+        }
+        System.out.println("Não há espaço disponível na pilha.");
+        return true;
+    }
+
+    //Função que encontra a posição do elemento na pilha
+    private int encontrarPosicao() {
+        int i;
+        for (i = 0; i < this.pilha.length; i++) {
+            if(this.pilha[i] == null) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    //Função que exibe os elementos da pilha (último --> primeiro)
+    public void exibirElementos() {
+        for (int i = this.pilha.length-1; i >= 0; i--) {
+            System.out.println("Pilha[" + i + "] = " + this.pilha[i]);
+        }
+    }
 
 }
