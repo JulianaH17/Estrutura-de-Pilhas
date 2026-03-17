2dd4451bf4601c4594f8de60ac21365119bfa0b7
Isabela <juliana.bando@edu.unifil.br>
2026-03-17 15:22:29 -0300

    Construindo a estrutura da PilhaDinamica, implementando os métodos de empilhar e exibir e criando a classe de teste MainPilhaDinamica

---
+++ src/MainPilhaDinamica.java
@@ -0,0 +1,15 @@
+public class MainPilhaDinamica {
+    public static void main(String[] args) {
+
+        PilhaDinamica pilhaDinamica = new PilhaDinamica();
+        pilhaDinamica.empilhar("Juliana");
+        pilhaDinamica.empilhar("Isabela");
+        pilhaDinamica.empilhar("Nicolas");
+        pilhaDinamica.empilhar("Caio");
+        pilhaDinamica.empilhar("Vitor");
+        pilhaDinamica.empilhar("Leticia");
+        pilhaDinamica.empilhar("Lucas");
+
+        pilhaDinamica.exibir();
+    }
+}
--- src/PilhaDinamica.java
+++ src/PilhaDinamica.java
@@ -1,2 +1,53 @@
 public class PilhaDinamica {
+    No inicio;
+
+    public PilhaDinamica() {
+        this.inicio = new No(null);
+        System.out.println("Pilha dinâmica criada com sucesso!");
+    }
+
+    public void empilhar(String elemento) {
+
+        if (elemento == null || elemento.trim().isEmpty()) {
+            System.out.println("Elemento inválido.");
+            return;
+        }
+
+        if (inicioEstaVazio()) {
+            this.inicio.setConteudo(elemento);
+        } else {
+            No aux = this.inicio;
+            No novoNo = new No(elemento);
+            while (aux.getProx() != null) {
+                aux = aux.getProx();
+            }
+            aux.setProx(novoNo);
+        }
+    }
+    private boolean inicioEstaVazio() {
+        if (this.inicio.getConteudo() == null) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void exibir() {
+        if (inicioEstaVazio()) {
+            System.out.println("Não existem elementos na lista dinâmica.");
+        } else {
+            No aux = this.inicio;
+//            while(aux != null && aux.getConteudo() != null) {
+//                System.out.println(aux.getConteudo());
+//                aux = aux.getProx();
+//            }
+
+            while (aux != null) {
+                System.out.println(aux.getConteudo());
+                aux = aux.getProx();
+            }
+            //System.out.println(aux.getConteudo());
+        }
+    }
+
 }
